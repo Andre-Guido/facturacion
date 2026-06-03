da2b86fccca58b222e22dbf4aa1ee16a34164a6f
Andre <andre.guido@uamv.edu.ni>
2026-06-03 16:01:51 -0600

    feat: Añadiendo las clases y funcionalidades de Factura y Detalle del paquete modelos. Añadiendo el paquete calculadores con la clase CalculadorSiguienteNumeroParaAnyo. Añadiendo la etiqueta para la propiedad producto.numero -> producto.numero = N° de Producto.

---
+++ src/main/java/com/tuempresa/facturacion/modelo/Factura.java
@@ -0,0 +1,45 @@
+package com.tuempresa.facturacion.modelo;
+
+import com.tuempresa.facturacion.calculadores.CalculadorSiguienteNumeroParaAnyo;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.openxava.annotations.*;
+import org.openxava.calculators.CurrentLocalDateCalculator;
+import org.openxava.calculators.CurrentYearCalculator;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Collection;
+
+@Entity @Getter @Setter
+public class Factura {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @Hidden
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    @Column(length = 32)
+    private String oid;
+
+    @Column(length = 4)
+    @DefaultValueCalculator(CurrentYearCalculator.class)
+    int anyo;
+
+    @Column(length = 6)
+    @DefaultValueCalculator(value = CalculadorSiguienteNumeroParaAnyo.class, properties = @PropertyValue(name = "anyo"))
+    int numero;
+
+    @Required
+    @DefaultValueCalculator(CurrentLocalDateCalculator.class)
+    LocalDate fecha;
+
+    @TextArea
+    String observaciones;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    Cliente cliente;
+
+    @ElementCollection
+    @ListProperties("producto.numero, producto,descripcion, cantidad")
+    Collection<Detalle> detalles;
+}
